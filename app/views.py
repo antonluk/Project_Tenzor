56d1ce560a7da05da1574423db4b1d6d4c614159
webyart <webyart@yandex.ru>
2019-11-18 14:22:33 +0500

    vitamsovsemohueli?

---
+++ app/views.py
@@ -0,0 +1,146 @@
+import calendar
+
+from flask_appbuilder import ModelView
+from flask_appbuilder.charts.views import GroupByChartView
+from flask_appbuilder.models.group import aggregate_count
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+
+from . import appbuilder, db
+from .models import Contact, ContactGroup, Gender, Product
+
+
+def fill_gender():
+    try:
+        db.session.add(Gender(name="Male"))
+        db.session.add(Gender(name="Female"))
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+
+
+class ContactModelView(ModelView):
+    datamodel = SQLAInterface(Contact)
+
+    list_columns = ["name", "personal_celphone", "birthday", "contact_group.name"]
+
+    base_order = ("name", "asc")
+    show_fieldsets = [
+        ("Summary", {"fields": ["name", "gender", "contact_group"]}),
+        (
+            "Personal Info",
+            {
+                "fields": [
+                    "address",
+                    "birthday",
+                    "personal_phone",
+                    "personal_celphone",
+                ],
+                "expanded": False,
+            },
+        ),
+    ]
+
+    add_fieldsets = [
+        ("Summary", {"fields": ["name", "gender", "contact_group"]}),
+        (
+            "Personal Info",
+            {
+                "fields": [
+                    "address",
+                    "birthday",
+                    "personal_phone",
+                    "personal_celphone",
+                ],
+                "expanded": False,
+            },
+        ),
+    ]
+
+    edit_fieldsets = [
+        ("Summary", {"fields": ["name", "gender", "contact_group"]}),
+        (
+            "Personal Info",
+            {
+                "fields": [
+                    "address",
+                    "birthday",
+                    "personal_phone",
+                    "personal_celphone",
+                ],
+                "expanded": False,
+            },
+        ),
+    ]
+
+
+class GroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+    related_views = [ContactModelView]
+
+
+def pretty_month_year(value):
+    return calendar.month_name[value.month] + " " + str(value.year)
+
+
+def pretty_year(value):
+    return str(value.year)
+
+
+class ContactTimeChartView(GroupByChartView):
+    datamodel = SQLAInterface(Contact)
+
+    chart_title = "Grouped Birth contacts"
+    chart_type = "AreaChart"
+    label_columns = ContactModelView.label_columns
+    definitions = [
+        {
+            "group": "month_year",
+            "formatter": pretty_month_year,
+            "series": [(aggregate_count, "group")],
+        },
+        {
+            "group": "year",
+            "formatter": pretty_year,
+            "series": [(aggregate_count, "group")],
+        },
+    ]
+
+
+class ProductModelView(ModelView):
+    datamodel = SQLAInterface(Product)
+
+    list_columns = ["name", "price"]
+
+    base_order = ("name", "price")
+
+    show_fieldsets = [
+        ("Summary", {"fields": ["name", "price"]})
+    ]
+
+    add_fieldsets = [
+        ("Summary", {"fields": ["name", "price"]})
+    ]
+
+    edit_fieldsets = [
+        ("Summary", {"fields": ["name", "price"]})
+    ]
+
+
+db.create_all()
+fill_gender()
+appbuilder.add_view(
+    GroupModelView, "List Groups", icon="", category="Contacts", category_icon="",
+)
+appbuilder.add_view(
+    ContactModelView, "List Contacts", icon="", category="Contacts"
+)
+
+# appbuilder.add_separator("Contacts")
+# appbuilder.add_view(
+#     ContactTimeChartView, "Contacts Birth Chart", icon="fa-dashboard", category="Contacts",
+# )
+
+# appbuilder.add_separator("Products")
+appbuilder.add_view(
+    ProductModelView, "Product", icon="", category="Products",
+)
---
+++ config.py
@@ -0,0 +1,116 @@
+import os
+from flask_appbuilder.security.manager import (
+    AUTH_OID,
+    AUTH_REMOTE_USER,
+    AUTH_DB,
+    AUTH_LDAP,
+    AUTH_OAUTH,
+)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Your App secret key
+SECRET_KEY = "testToken"
+
+
+# HEROKU CONNECT
+PG_USER = "yxurhrhiytlgax"
+PG_PSWD = "3bf59d9cbab1123322cef4d5b81a67ef3eb20e13b56f550068483ae94c394e73"
+PG_HOST = "ec2-54-75-245-196.eu-west-1.compute.amazonaws.com"
+PG_DATABASE = "db2msbksnnf0pb"
+
+
+# The SQLAlchemy connection string.
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+# SQLALCHEMY_DATABASE_URI = "postgresql://"+PG_USER+":"+PG_PSWD+"@"+PG_HOST+"/"+PG_DATABASE
+
+# Flask-WTF flag for CSRF
+CSRF_ENABLED = True
+
+# ------------------------------
+# GLOBALS FOR APP Builder
+# ------------------------------
+# Uncomment to setup Your App name
+APP_NAME = "DeliveryGram"
+
+# Uncomment to setup Setup an App icon
+# APP_ICON = "static/img/logo.jpg"
+
+# ----------------------------------------------------
+# AUTHENTICATION CONFIG
+# ----------------------------------------------------
+# The authentication type
+# AUTH_OID : Is for OpenID
+# AUTH_DB : Is for database (username/password()
+# AUTH_LDAP : Is for LDAP
+# AUTH_REMOTE_USER : Is for using REMOTE_USER from web server
+AUTH_TYPE = AUTH_DB
+
+# Uncomment to setup Full admin role name
+# AUTH_ROLE_ADMIN = 'Admin'
+
+# Uncomment to setup Public role name, no authentication needed
+# AUTH_ROLE_PUBLIC = 'Public'
+
+# Will allow user self registration
+# AUTH_USER_REGISTRATION = True
+
+# The default user self registration role
+# AUTH_USER_REGISTRATION_ROLE = "Public"
+
+# When using LDAP Auth, setup the ldap server
+# AUTH_LDAP_SERVER = "ldap://ldapserver.new"
+
+# Uncomment to setup OpenID providers example for OpenID authentication
+# OPENID_PROVIDERS = [
+#    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+#    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+#    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+#    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+# ---------------------------------------------------
+# Babel config for translations
+# ---------------------------------------------------
+# Setup default language
+BABEL_DEFAULT_LOCALE = "ru"
+# Your application default translation path
+BABEL_DEFAULT_FOLDER = "translations"
+# The allowed translation for you app
+LANGUAGES = {
+    "ru": {"flag": "ru", "name": "Russian"},
+    "en": {"flag": "gb", "name": "English"},
+}
+# ---------------------------------------------------
+# Image and file configuration
+# ---------------------------------------------------
+# The file upload folder, when using models with files
+UPLOAD_FOLDER = basedir + "/app/static/uploads/"
+
+# The image upload folder, when using models with images
+IMG_UPLOAD_FOLDER = basedir + "/app/static/uploads/"
+
+# The image upload url, when using models with images
+IMG_UPLOAD_URL = "/static/uploads/"
+# Setup image size default is (300, 200, True)
+# IMG_SIZE = (300, 200, True)
+
+# Theme configuration
+# these are located on static/appbuilder/css/themes
+# you can create your own and easily use them placing them on the same dir structure to override
+# APP_THEME = "bootstrap-theme.css"  # default bootstrap
+# APP_THEME = "cerulean.css"
+# APP_THEME = "amelia.css"
+# APP_THEME = "cosmo.css"
+# APP_THEME = "cyborg.css"
+# APP_THEME = "flatly.css"
+# APP_THEME = "journal.css"
+# APP_THEME = "readable.css"
+# APP_THEME = "simplex.css"
+# APP_THEME = "slate.css"
+# APP_THEME = "spacelab.css"
+# APP_THEME = "united.css"
+# APP_THEME = "yeti.css"
+
+# ------------------------------
+# GLOBALS FOR GENERAL APP's
+# ------------------------------
+FAB_API_SWAGGER_UI = True
